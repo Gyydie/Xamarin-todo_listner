6ca0cbcb3860ba1509fbd63901c68ac4b280973b
Denis Arkhipov <denarxipow@yandex.ru>
2021-05-23 00:00:19 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/Views/CategoriesPage1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using App1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoriesPage1 : ContentPage
+    {
+        private CategoriesViewModel _viewModel;
+
+        public CategoriesPage1()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new CategoriesViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
